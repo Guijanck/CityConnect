9db3bc111baf25f749fbcf439d9b017b2106968d
Diego Juan Isaquiel <diego.mizael@facens.br>
2023-09-07 11:47:13 -0300

    feat(footer):Setting routes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { PageEnum } from 'src/app/models/enums/pages.enum';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent  implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
+  //#region Constructor Region
+  constructor(
+    private router: Router,
+  ) {
+    this.setCurrentPage(this.router.url.split('/')[2]);
+  }
 
-  ngOnInit() {}
+  //#endregion
 
+  //#region Public Properties
+
+  public pageEnum: typeof PageEnum = PageEnum;
+
+  public currentPage: number = 0;
+
+  //#endregion
+
+  //#region Public Methods
+
+  public async navigateTo(url: string): Promise<void> {
+    await this.router.navigate(['/main/' + url]);
+    await this.setCurrentPage(url);
+  }
+
+  public setCurrentPage(url: string): void {
+    switch (url) {
+      case 'home':
+        this.currentPage = this.pageEnum.HOME;
+        break;
+      case 'events':
+        this.currentPage = this.pageEnum.EVENTS;
+        break;
+      case 'add-report':
+        this.currentPage = this.pageEnum.ADD_REPORT;
+        break;
+      case 'ranking':
+        this.currentPage = this.pageEnum.RANKING;
+        break;
+      case 'profile':
+        this.currentPage = this.pageEnum.PROFILE;
+        break;
+    }
+  }
+
+  //#endregion
 }
---
+++ src/app/models/enums/pages.enum.ts
@@ -0,0 +1,7 @@
+export enum PageEnum {
+	HOME = 1,
+	EVENTS = 2,
+	ADD_REPORT = 3,
+	RANKING = 4,
+	PROFILE = 5
+}
